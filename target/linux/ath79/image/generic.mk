jacelift/x-wrt_jacelift

ath79: init support for bm100_hq55
This also support bm100_hq55-wb

Signed-off-by: Chen Minqiang <ptpt52@gmail.com>

target/linux/ath79/image/generic.mk
@@ -71,6 +71,17 @@ define Build/pisen_wmb001n-factory
   rm -rf "$@.tmp"
 endef
 
+define Build/bm100_hq55-loader-factory
+  -cat "$(KDIR)/loader-$(word 1,$(1)).uImage" >"$@"
+endef
+
+define Build/bm100_hq55-uboot-factory
+  -[ -f "$@" ] && \
+  dd if="$(KDIR)/loader-$(word 1,$(1)).uImage" of="$@".tmp bs=64k conv=sync && \
+  cat "$@" >>"$@".tmp && \
+  mv "$@".tmp "$@"
+endef
+
 define Build/xwrt_csac-factory
   -[ -f "$@" ] && \
   mkdir -p "$@.tmp" && \
@@ -186,6 +197,24 @@ define Device/avm_fritz4020
   SUPPORTED_DEVICES += fritz4020
 endef
 TARGET_DEVICES += avm_fritz4020
+
+define Device/bm100_hq55
+  ATH_SOC := ar9344
+  DEVICE_VENDOR := BM100
+  DEVICE_MODEL := HQ55
+  DEVICE_PACKAGES := kmod-usb-core kmod-usb2 kmod-usb-ledtrig-usbport kmod-leds-gpio
+  IMAGE_SIZE := 15744k
+  LOADER_TYPE := bin
+  LOADER_FLASH_OFFS := 0x60000
+  COMPILE := loader-$(1).bin loader-$(1).uImage
+  COMPILE/loader-$(1).bin := loader-okli-compile
+  COMPILE/loader-$(1).uImage := append-loader-okli $(1) | pad-to 64k | lzma | uImage lzma
+  KERNEL := kernel-bin | append-dtb | lzma | uImage lzma -M 0x4f4b4c49
+  IMAGES += loader-factory.bin uboot-factory.bin
+  IMAGE/loader-factory.bin := bm100_hq55-loader-factory $(1)
+  IMAGE/uboot-factory.bin := $$(IMAGE/sysupgrade.bin) | bm100_hq55-uboot-factory $(1)
+endef
+TARGET_DEVICES += bm100_hq55
 
 define Device/buffalo_bhr-4grv
   ATH_SOC := ar7242

samara1532/x-wrt_my

target/linux/ath79/image/generic.mk
@@ -685,6 +685,27 @@ define Device/belkin_f9k1115-v2
 endef
 TARGET_DEVICES += belkin_f9k1115-v2
 
+define Build/append-rootfs-64k
+	dd if=$(IMAGE_ROOTFS) bs=64k count=1 >> $@
+endef
+
+define Device/bm100_hq55
+  $(Device/loader-okli-uimage)
+  SOC := ar9344
+  DEVICE_VENDOR := BM100
+  DEVICE_MODEL := HQ55
+  DEVICE_PACKAGES := kmod-usb-core kmod-usb2 kmod-usb-ledtrig-usbport kmod-leds-gpio
+  IMAGE_SIZE := 14656k
+  LOADER_FLASH_OFFS := 0x60000
+  KERNEL := kernel-bin | append-dtb | lzma | uImage lzma -M 0x4f4b4c49
+  IMAGES += loader-factory.bin uboot-factory.bin breed-factory.bin
+  IMAGE/loader-factory.bin := append-okli-kernel $(1)
+  IMAGE/uboot-factory.bin := append-okli-kernel $(1) | pad-to 64k | append-kernel | pad-to $$$$(BLOCKSIZE) | append-rootfs | pad-rootfs
+  IMAGE/breed-factory.bin := append-rootfs-64k | append-kernel | pad-to $$$$(BLOCKSIZE) | append-rootfs | pad-rootfs | \
+			     pad-to 14720k | append-okli-kernel $(1)
+endef
+TARGET_DEVICES += bm100_hq55
+
 define Device/buffalo_bhr-4grv
   $(Device/buffalo_common)
   SOC := ar7242